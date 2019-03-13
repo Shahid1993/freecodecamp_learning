f9226a8ae6bd0d3c02fb745917ade6cd8804e145
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-13 15:50:39 +0530

    Create basicDataStructures.js

---
+++ basicDataStructures.js
@@ -0,0 +1,11 @@
+/** Add Items to an Array with push() and unshift() */
+/** the push() method adds elements to the end of an array, and unshift() adds elements to the beginning */
+
+let twentyThree = 'XXIII';
+let romanNumerals = ['XXI', 'XXII'];
+
+romanNumerals.unshift('XIX', 'XX');
+// now equals ['XIX', 'XX', 'XXI', 'XXII']
+
+romanNumerals.push(twentyThree);
+// now equals ['XIX', 'XX', 'XXI', 'XXII', 'XXIII']
