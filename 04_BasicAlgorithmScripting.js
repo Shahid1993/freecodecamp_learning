d8910594c70168e198c25776fbb79ed33df44a3b
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-13 19:58:58 +0530

    Update 04_BasicAlgorithmScripting.js

--- 04_BasicAlgorithmScripting.js
+++ 04_BasicAlgorithmScripting.js
@@ -1,1 +1,37 @@
+/** Reverse a String */
 
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
+
+
+
+
+/** Factorialize a Number */
+
+function factorialize(num) {
+  if(num == 0 )
+    return 1;
+  return num * factorialize(num-1);
+}
+
+factorialize(5);
+
+
+
+
+/** Find the Longest Word in a String */
+
+function findLongestWordLength(str) {
+  return str.split(' ')
+    .reduce(function(x,y){
+      return Math.max(x,y.length);
+    },0);
+}
+
+
+
+
+/**  */
