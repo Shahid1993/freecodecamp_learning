f41d0da14bb69500d27876ba264acde481e91953
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-13 16:10:02 +0530

    Update basicDataStructures.js

--- basicDataStructures.js
+++ basicDataStructures.js
@@ -9,3 +9,31 @@ romanNumerals.unshift('XIX', 'XX');
 
 romanNumerals.push(twentyThree);
 // now equals ['XIX', 'XX', 'XXI', 'XXII', 'XXIII']
+
+
+
+
+/** Remove Items from an Array with pop() and shift() */
+/** pop() removes an element from the end of an array, while shift() removes an element from the beginning. */
+
+let greetings = ['whats up?', 'hello', 'see ya!'];
+
+greetings.pop();
+// now equals ['whats up?', 'hello']
+
+greetings.shift();
+// now equals ['hello']
+
+
+
+
+/** Remove Items Using splice() */
+/** splice(startIndex, length) : to remove an element/elements from somewhere in the middle */
+/** splice(startIndex, length, replaceElement) : to remove an element/elements from somewhere in the middle and replace */
+
+
+let array = ['today', 'was', 'not', 'so', 'great'];
+
+array.splice(2, 2);
+// remove 2 elements beginning with the 3rd element
+// array now equals ['today', 'was', 'great']
