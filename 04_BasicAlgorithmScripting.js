8301fe67bd04fe5782b008e279d54dc97872da5b
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-14 18:30:15 +0530

    Update 04_BasicAlgorithmScripting.js

--- 04_BasicAlgorithmScripting.js
+++ 04_BasicAlgorithmScripting.js
@@ -201,4 +201,21 @@ bouncer([7, "ate", "", false, 9]);
 
 
 
-/**  */
+/** Where do I Belong */
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+function getIndexToIns(arr, num) {
+  return arr.concat(num).sort((a,b)=> a-b).indexOf(num);
+}
+
+// or
+
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  arr.sort(function(a, b){return a-b});
+  return arr.indexOf(num);
+}
+
+// or ===> https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/basic-algorithm-scripting/where-do-i-belong/
+
+getIndexToIns([40, 60], 50);
