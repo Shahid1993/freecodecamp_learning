31f3921b9ee7eaaedd3d851dfedf92ca0bfc6bf0
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-14 11:47:31 +0530

    Update 04_BasicAlgorithmScripting.js

--- 04_BasicAlgorithmScripting.js
+++ 04_BasicAlgorithmScripting.js
@@ -34,4 +34,20 @@ function findLongestWordLength(str) {
 
 
 
-/**  */
+/** Return Largest Numbers in Arrays */
+
+function largestOfFour(arr) {
+  return arr.map(Function.apply.bind(Math.max,null));
+}
+
+// or 
+
+function largestOfFour(arr) {
+  return arr.map(function(subArr){
+    return subArr.reduce(function(prev, curr){
+      return curr >= prev ? curr : prev;
+    })
+  })
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
