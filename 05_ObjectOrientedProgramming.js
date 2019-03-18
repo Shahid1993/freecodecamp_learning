9974e2b768765f6ca3fff9d30dc12c47f515a171
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-18 19:28:02 +0530

    Update 05_ObjectOrientedProgramming.js

--- 05_ObjectOrientedProgramming.js
+++ 05_ObjectOrientedProgramming.js
@@ -1,1 +1,11 @@
 
+/** Define a Constructor Function */
+//Constructors are defined with a capitalized name to distinguish them from other functions that are not constructors.
+//Constructors use the keyword this to set properties of the object they will create. Inside the constructor, this refers to the new object it will create.
+//Constructors define properties and behaviors instead of returning a value as other functions might.
+
+function Dog(){
+  this.name = 'name';
+  this.color = 'color';
+  this.numLegs = 4;
+}
