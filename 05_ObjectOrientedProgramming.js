772e1e1ce80b294ff6dbdacc6349c626e618a342
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-18 19:33:47 +0530

    Update 05_ObjectOrientedProgramming.js

--- 05_ObjectOrientedProgramming.js
+++ 05_ObjectOrientedProgramming.js
@@ -9,3 +9,23 @@ function Dog(){
   this.color = 'color';
   this.numLegs = 4;
 }
+
+
+
+
+/** Verify an Object's Constructor with instanceof */
+// instanceof allows you to compare an object to a constructor, returning true or false based on whether or not that object was created with the constructor.
+let Bird = function(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 2;
+}
+
+let crow = new Bird("Alexis", "black");
+
+crow instanceof Bird; // => true
+
+
+
+
+/** 
