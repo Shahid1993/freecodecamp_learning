c4c253c2d657bc152b5c7acabfb722776d53e0db
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-04-02 20:18:32 +0530

    Update 05_ObjectOrientedProgramming.js

--- 02_JSAlgoAndDS/05_ObjectOrientedProgramming.js
+++ 02_JSAlgoAndDS/05_ObjectOrientedProgramming.js
@@ -32,6 +32,11 @@ crow instanceof Bird; // => true
 // name and numLegs are called own properties, because they are defined directly on the instance object. That means that duck and canary each has its own separate copy of these properties.
 // In fact every instance of Bird will have its own copy of these properties.
 
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
 let ownProps = [];
 
 for (let property in duck) {
