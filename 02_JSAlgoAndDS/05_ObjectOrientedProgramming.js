a248e3a4e977b6580acdd108c8effc47db508eea
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-04-10 14:04:13 +0530

    Update 05_ObjectOrientedProgramming.js

--- 02_JSAlgoAndDS/05_ObjectOrientedProgramming.js
+++ 02_JSAlgoAndDS/05_ObjectOrientedProgramming.js
@@ -87,4 +87,224 @@ console.log(prototypeProps); // prints ["numLegs"]
 /**  constructor property is a reference to the constructor function that created the instance. */
 /** The advantage of the constructor property is that it's possible to check for this property to find out what kind of object it is. */
 
+function Dog(name) {
+  this.name = name;
+}
 
+// Add your code below this line
+function joinDogFraternity(candidate) {
+  return candidate.constructor === Dog;
+}
+
+
+
+
+/** Change the Prototype to a New Object **/
+
+Bird.prototype = {
+  numLegs: 2,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
+
+console.log(duck.constructor)   // prints ‘undefined’ - Oops!  -- constructor property got erased
+// To fix this, whenever a prototype is manually set to a new object, remember to define the constructor property
+
+
+
+
+/** Remember to Set the Constructor Property when Changing the Prototype **/
+
+Bird.prototype = {
+  constructor: Bird, // define the constructor property
+  numLegs: 2,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
+
+
+
+
+/** Understand Where an Object’s Prototype Comes From **/
+//an object inherits its prototype directly from the constructor function that created it.
+
+function Bird(name) {
+  this.name = name;
+}
+
+let duck = new Bird("Donald");
+//duck inherits its prototype from the Bird constructor function. You can show this relationship with the isPrototypeOf method:
+
+Bird.prototype.isPrototypeOf(duck);
+// returns true
+
+
+
+
+/** Understand the Prototype Chain **/
+//All objects in JavaScript (with a few exceptions) have a prototype. Also, an object’s prototype itself is an object.
+
+function Bird(name) {
+  this.name = name;
+}
+
+typeof Bird.prototype; // => object
+
+//Because a prototype is an object, a prototype can have its own prototype! 
+//In this case, the prototype of Bird.prototype is Object.prototype:
+
+Object.prototype.isPrototypeOf(Bird.prototype);  // returns true
+
+//The hasOwnProperty method is defined in Object.prototype, which can be accessed by Bird.prototype, which can then be accessed by duck. This is an example of the prototype chain.
+//In this prototype chain, Bird is the supertype for duck, while duck is the subtype. Object is a supertype for both Bird and duck.
+//Object is a supertype for all objects in JavaScript. Therefore, any object can use the hasOwnProperty method.
+
+
+
+
+
+/** Use Inheritance So You Don't Repeat Yourself **/
+//There's a principle in programming called Don't Repeat Yourself (DRY).
+
+function Cat(name) {
+  this.name = name; 
+}
+
+Cat.prototype = {
+  constructor: Cat
+};
+
+function Bear(name) {
+  this.name = name; 
+}
+
+Bear.prototype = {
+  constructor: Bear
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+
+
+
+/** Inherit Behaviors from a Supertype **/
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal, 
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+//one way to create an instance of Animal using the new operator:
+let duck = new Animal();
+//There are some disadvantages when using this syntax for inheritance. Instead, here's an alternative approach without those disadvantages:
+let duck =  Object.create(Animal.prototype); // Change this line
+let beagle = Object.create(Animal.prototype); // Change this line
+
+duck.eat(); // Should print "nom nom nom"
+beagle.eat(); // Should print "nom nom nom" 
+//Object.create(obj) creates a new object, and sets obj as the new object's prototype.
+
+
+
+
+
+/** Set the Child's Prototype to an Instance of the Parent **/
+//set the prototype of the subtype (or child)—in this case, Bird—to be an instance of Animal.
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Add your code below this line
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
+beagle.eat();  // Should print "nom nom nom"
+
+
+
+
+/** Reset an Inherited Constructor Property **/
+//When an object inherits its prototype from another object, it also inherits the supertype's constructor property.
+
+function Bird() { }
+Bird.prototype = Object.create(Animal.prototype);
+let duck = new Bird();
+duck.constructor // function Animal(){...}
+
+//But duck and all instances of Bird should show that they were constructed by Bird and not Animal. 
+//To do so, you can manually set Bird's constructor property to the Bird object:
+
+Bird.prototype.constructor = Bird;
+duck.constructor // function Bird(){...}
+
+
+
+
+/** Add Methods After Inheritance **/
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Add your code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function(){
+    console.log("Woof!");
+}
+
+let beagle = new Dog();
+
+beagle.eat(); // Should print "nom nom nom"
+beagle.bark(); // Should print "Woof!"
+
+
+
+
+/** Override Inherited Methods **/
+
+function Animal() { }
+Animal.prototype.eat = function() {
+  return "nom nom nom";
+};
+function Bird() { }
+
+// Inherit all methods from Animal
+Bird.prototype = Object.create(Animal.prototype);
+
+// Bird.eat() overrides Animal.eat()
+Bird.prototype.eat = function() {
+  return "peck peck peck";
+};
+
+
+
+
+/** Use a Mixin to Add Common Behavior Between Unrelated Objects **/
